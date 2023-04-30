1616c55b06417415da5ff75262211ff5f94fd288
Bruno Duarte <brunoduarte96@outlook.com>
2023-04-30 16:38:18 -0300

    CORS e teste de integração

--- src/main/java/br/com/duarte/config/WebConfig.java
+++ src/main/java/br/com/duarte/config/WebConfig.java
@@ -2,10 +2,12 @@ package br.com.duarte.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import br.com.duarte.serialization.converter.YamlJackson2HttpMessageConverter;
@@ -13,36 +15,52 @@ import br.com.duarte.serialization.converter.YamlJackson2HttpMessageConverter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Override
-	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+    private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
 
-		final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
-		
-		configurer.favorParameter(false).ignoreAcceptHeader(false).useRegisteredExtensionsOnly(false)
-		.defaultContentType(MediaType.APPLICATION_JSON).mediaType("json", MediaType.APPLICATION_JSON)
-		.mediaType("xml", MediaType.APPLICATION_XML).mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
-	
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
 
-		/*
-		 * configurer.favorParameter(true)
-		 * .parameterName("mediaType").ignoreAcceptHeader(true)
-		 * .useRegisteredExtensionsOnly(false)
-		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
-		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
-		 */
 
-		
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new YamlJackson2HttpMessageConverter());
+    }
 
-	}
 
-	@Override
-	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-		converters.add(new YamlJackson2HttpMessageConverter());
-	}
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
 
-	
-		
-	}
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                //.allowedMethods("GET", "POST","PUT")
+                .allowedMethods("*")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
+
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+
+        configurer.favorParameter(false).ignoreAcceptHeader(false).useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON).mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML).mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
+
+
+        /*
+         * configurer.favorParameter(true)
+         * .parameterName("mediaType").ignoreAcceptHeader(true)
+         * .useRegisteredExtensionsOnly(false)
+         * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
+         * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
+         */
+
+
+    }
+
+
+}
 
 
 
--- src/main/java/br/com/duarte/controller/PersonController.java
+++ src/main/java/br/com/duarte/controller/PersonController.java
@@ -4,14 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.duarte.data.vo.v1.PersonVO;
 import br.com.duarte.services.PersonService;
@@ -45,6 +38,7 @@ public class PersonController {
 		return service.findAll();
 	}
 
+	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
 			MediaType.APPLICATION_YML })
 
@@ -62,6 +56,7 @@ public class PersonController {
 		return service.findById(id);
 	}
 
+	@CrossOrigin(origins = {"http://localhost:8080","https://erudio.com.br"})
 	@PostMapping(consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
 			MediaType.APPLICATION_YML }, produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
 					MediaType.APPLICATION_YML })
