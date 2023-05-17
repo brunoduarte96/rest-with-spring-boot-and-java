3b2b3af604db5275673b928693f454f201a4bd3b
Bruno Duarte <brunoduarte96@outlook.com>
2023-05-16 21:26:59 -0300

    UserRepository e UserServicer

---
+++ src/main/java/br/com/duarte/services/UserService.java
@@ -0,0 +1,43 @@
+package br.com.duarte.services;
+
+import br.com.duarte.controller.PersonController;
+import br.com.duarte.data.vo.v1.PersonVO;
+import br.com.duarte.exceptions.ResourceNotFoundException;
+import br.com.duarte.mapper.DozerMapper;
+import br.com.duarte.model.Person;
+import br.com.duarte.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Service
+public class UserService implements UserDetailsService {
+    private Logger logger = Logger.getLogger(UserService.class.getName());
+
+    @Autowired
+    UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.info("Finding one user by name " + username + "!");
+        var user = repository.findByUsername(username);
+        if (user != null) {
+            return user;
+        } else {
+            throw new UsernameNotFoundException("Username" + username + "not found!");
+        }
+    }
+}
