d097f5f6d76c1b64c54779655a380130ef873688
Bruno Duarte <brunoduarte96@outlook.com>
2023-05-16 21:36:35 -0300

    Exception personalizada, AccountCredentialsVO e TokenVO

--- src/main/java/br/com/duarte/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/br/com/duarte/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -1,7 +1,8 @@
 package br.com.duarte.exceptions.handler;
 
-import java.util.Date;
-
+import br.com.duarte.exceptions.ExceptionResponse;
+import br.com.duarte.exceptions.InvalidJwtAuthenticationException;
+import br.com.duarte.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,8 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import br.com.duarte.exceptions.ExceptionResponse;
-import br.com.duarte.exceptions.ResourceNotFoundException;
+import java.util.Date;
 
 @ControllerAdvice
 @RestController
@@ -39,6 +39,17 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 				request.getDescription(false));
 		
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationException(
+			Exception ex, WebRequest request) {
+
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+				new Date(),
+				ex.getMessage(),
+				request.getDescription(false));
+
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
 	}
 
 }
